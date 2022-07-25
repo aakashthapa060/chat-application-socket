4a0bb9a949684a4c908092b20c374c158b03b10d
Aakash Thapa <aakashmanm3@gmail.com>
2022-07-25 10:43:30 +0545

    From Socket Docs

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+    socket.on("chat message", (msg) => {
+        io.emit("chat message", msg);
+    })
+});
+
+const PORT = 8000;
+server.listen(PORT, () => {
+  console.log('listening on *:8000');
+});
